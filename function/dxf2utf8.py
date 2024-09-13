cdf5416f76211784bf7c02db2a60e01ee16df3c0
ytkz11 <ytkz11@163.com>
2024-09-14 00:12:09 +0800

    test

--- function/dxf2utf8.py
+++ function/dxf2utf8.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-# @Time : 2024/9/13 17:46 
-# @File : dxf2utf8.py 
-'''
+# -*- coding: utf-8 -*-
+# @Time : 2024/1/3 20:32
+# @File : RestoreShp.py
 
-'''
+import dbf
+from io import StringIO
+
 import dbf
 import pandas as pd
 
-# 读取.dbf文件
-table = dbf.Table(r'D:\temp\out\84\Fisheries_20190927.dbf')
-table.open()
-records = [r.field_dict for r in table]
-table.close()
 
-# 将记录转换为DataFrame并保存为CSV
-df = pd.DataFrame(records)
-df.to_csv('temp.csv', index=False, encoding='utf-8')
 
-# 重新从CSV读取并转换回.dbf
-df = pd.read_csv('temp.csv')
-new_table = dbf.Table('D:\temp\out\84\Fisheries_201909271.dbf', df.columns.str.replace('.', ''))
-new_table.open(mode=dbf.WN)
-for i, row in df.iterrows():
-    new_table.append(row.values.tolist())
-new_table.close()
 
+
+
+if __name__ == '__main__':
+    # 使用函数
+    input_dbf = r'C:\Users\Administrator\Documents\WeChat Files\wxid_ejyl8luu57t121\FileStorage\File\2024-09\Fisheries_20190927\2\Fisheries_20190927.dbf'
+    import dbfread
+
+    # 指定编码为 GBK (如果文件是用 GBK 编码)
+    table = dbfread.DBF(input_dbf, encoding='gbk')
+    for record in table:
+        print(record)
+
+    # 调用函数
+    # convert_encoding(input_dbf, output_dbf)
